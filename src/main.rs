79c07d66dd6d42121f4046808fe652219e503ac5
Sam Title <samstitle@gmail.com>
2025-02-03 23:19:49 -0500

    Factoring it out

--- src/main.rs
+++ src/main.rs
@@ -8,83 +8,88 @@ fn main() -> glib::ExitCode {
         .application_id("org.example.HelloWorld")
         .build();
 
-    app.connect_activate(|app| {
-        // We create the main window.
-        let window = ApplicationWindow::builder()
-            .application(app)
-            .default_width(320)
-            .default_height(200)
-            .title("Counter")
-            .build();
-
-        // Create a vertical box to hold the label and buttons.
-        let vbox = gtk::Box::new(Orientation::Vertical, 5);
-        vbox.set_margin_top(20);
-        vbox.set_margin_bottom(20);
-        vbox.set_margin_start(20);
-        vbox.set_margin_end(20);
-        vbox.set_halign(Align::Center);
-        vbox.set_valign(Align::Center);
-
-        // Create a label with large font and initial color.
-        let label = Label::new(Some("0"));
-        label.set_markup("<span size='xx-large' foreground='black'>0</span>");
-        vbox.append(&label);
-
-        // Create an increment button.
-        let increment_button = Button::with_label("Increment");
-        vbox.append(&increment_button);
-
-        // Connect increment button click event to increment the label and change color.
-        increment_button.connect_clicked({
-            let label = label.clone();
-            move |_| {
-                let current_value: i32 = label.text().parse().unwrap_or(0);
-                label.set_markup(&format!(
-                    "<span size='xx-large' foreground='{}'>{}</span>",
-                    random_color(),
-                    current_value + 1
-                ));
-            }
-        });
-
-        // Create a decrement button.
-        let decrement_button = Button::with_label("Decrement");
-        vbox.append(&decrement_button);
-
-        // Connect decrement button click event to decrement the label and change color.
-        decrement_button.connect_clicked({
-            let label = label.clone();
-            move |_| {
-                let current_value: i32 = label.text().parse().unwrap_or(0);
-                label.set_markup(&format!(
-                    "<span size='xx-large' foreground='{}'>{}</span>",
-                    random_color(),
-                    current_value - 1
-                ));
-            }
-        });
-
-        // Create a reset button.
-        let reset_button = Button::with_label("Reset");
-        vbox.append(&reset_button);
-
-        // Connect reset button click event to reset the label and change color.
-        reset_button.connect_clicked({
-            let label = label.clone();
-            move |_| {
-                label.set_markup("<span size='xx-large' foreground='black'>0</span>");
-            }
-        });
-
-        // Add the vbox to the window.
-        window.set_child(Some(&vbox));
-
-        // Show the window.
-        window.present();
-    });
+    app.connect_activate(build_ui);
 
     app.run()
+}
+
+fn build_ui(app: &Application) {
+    let window = create_main_window(app);
+    let vbox = create_vbox();
+
+    let label = create_label();
+    vbox.append(&label);
+
+    let increment_button = create_button("Increment");
+    connect_button(&increment_button, &label, 1);
+    vbox.append(&increment_button);
+
+    let decrement_button = create_button("Decrement");
+    connect_button(&decrement_button, &label, -1);
+    vbox.append(&decrement_button);
+
+    let reset_button = create_button("Reset");
+    connect_reset_button(&reset_button, &label);
+    vbox.append(&reset_button);
+
+    window.set_child(Some(&vbox));
+    window.present();
+}
+
+fn create_main_window(app: &Application) -> ApplicationWindow {
+    ApplicationWindow::builder()
+        .application(app)
+        .default_width(320)
+        .default_height(200)
+        .title("Counter")
+        .build()
+}
+
+fn create_vbox() -> gtk::Box {
+    let vbox = gtk::Box::new(Orientation::Vertical, 5);
+    vbox.set_margin_top(20);
+    vbox.set_margin_bottom(20);
+    vbox.set_margin_start(20);
+    vbox.set_margin_end(20);
+    vbox.set_halign(Align::Center);
+    vbox.set_valign(Align::Center);
+    vbox
+}
+
+fn create_label() -> Label {
+    let label = Label::new(Some("0"));
+    label.set_markup(&format_label_markup("black", 0));
+    label
+}
+
+fn create_button(label: &str) -> Button {
+    Button::with_label(label)
+}
+
+fn connect_button(button: &Button, label: &Label, increment: i32) {
+    button.connect_clicked({
+        let label = label.clone();
+        move |_| {
+            let current_value: i32 = label.text().parse().unwrap_or(0);
+            label.set_markup(&format_label_markup(random_color().as_str(), current_value + increment));
+        }
+    });
+}
+
+fn connect_reset_button(button: &Button, label: &Label) {
+    button.connect_clicked({
+        let label = label.clone();
+        move |_| {
+            label.set_markup(&format_label_markup("black", 0));
+        }
+    });
+}
+
+fn format_label_markup(color: &str, value: i32) -> String {
+    format!(
+        "<span size='xx-large' foreground='{}'>{}</span>",
+        color, value
+    )
 }
 
 fn random_color() -> String {
